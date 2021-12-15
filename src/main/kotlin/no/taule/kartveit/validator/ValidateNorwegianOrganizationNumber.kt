3f7b199212d2432179f996a3a0281eda9c6500fb
Joakim Taule Kartveit <joakim.kartveit@edigard.no>
2021-12-15 10:54:00 +0100

    Added in test for checking if the OrganizationNumber is valid or not

--- src/main/kotlin/no/taule/kartveit/validator/ValidateNorwegianOrganizationNumber.kt
+++ src/main/kotlin/no/taule/kartveit/validator/ValidateNorwegianOrganizationNumber.kt
@@ -1,29 +1,30 @@
 package no.taule.kartveit.validator
 
-val lookup1: IntArray = intArrayOf(3, 7, 6, 1, 8, 9, 4, 5, 2, 0)
-val lookup2: IntArray = intArrayOf(5, 4, 3, 2, 7, 6, 5, 4, 3, 2)
+val weights: IntArray = intArrayOf(3, 2, 7, 6, 5, 4, 3, 2, 0)
 
-fun validateOrganizationNumberMod11(organizationNumber: String): Boolean {
-    if (organizationNumber.length != 11)
+fun validNorwegianOrganizationNumber(norwegianOrganizationNumber: String?): Boolean {
+    if (norwegianOrganizationNumber == null || norwegianOrganizationNumber.length != 9) {
         return false
-
-    var checksum1 = 0
-    var checksum2 = 0
-
-    for (i in 0..9) {
-        val currNum = (organizationNumber[i] - '0')
-        checksum1 += currNum * lookup1[i]
-        checksum2 += currNum * lookup2[i]
     }
 
-    checksum1 %= 11
-    checksum2 %= 11
+    return validatenorwegianOrganizationNumberMod11(norwegianOrganizationNumber)
 
-    val checksum1Final = if (checksum1 == 0) 0 else 11 - checksum1
-    val checksum2Final = if (checksum2 == 0) 0 else 11 - checksum2
+}
 
-    return checksum1Final != 10 &&
-            organizationNumber[9] - '0' == checksum1Final &&
-            organizationNumber[10] - '0' == checksum2Final
+fun validatenorwegianOrganizationNumberMod11(norwegianOrganizationNumber: String): Boolean {
+
+    var checksum = 0
+
+    for (i in 0..8) {
+        val currNum = (norwegianOrganizationNumber[i] - '0')
+        checksum += currNum * weights[i]
+    }
+
+    checksum %= 11
+
+    val checksumFinal = if (checksum == 0) 0 else 11 - checksum
+
+    return checksumFinal != 10 &&
+            norwegianOrganizationNumber[8] - '0' == checksumFinal
 }
 
--- src/test/kotlin/no/taule/kartveit/validator/ValidateNorwegianOrganizationNumberTest.kt
+++ src/test/kotlin/no/taule/kartveit/validator/ValidateNorwegianOrganizationNumberTest.kt
@@ -8,9 +8,50 @@ internal class ValidateNorwegianOrganizationNumberTest {
     @Test
     internal fun testShouldBeValidOrganizationNumber() {
 
-        //TODO
-        assertEquals(true, true)
+        validNorwegianOrganizationNumbers().forEach {
+            assertEquals(true, validNorwegianOrganizationNumber(it))
+        }
+
     }
 
+    @Test
+    internal fun testShouldBeInvalidOrganizationNumber() {
 
+        invalidNorwegianOrganizationNumbers().forEach {
+            assertEquals(false, validNorwegianOrganizationNumber(it))
+        }
+
+    }
+
+    fun validNorwegianOrganizationNumbers(): List<String> {
+        return listOf(
+            "123456785",
+            "135795313",
+            "999263550",
+            "999162681",
+            "889640782",
+            "998004993",
+            "974791854",
+            "921117795",
+            "917755736",
+            "995690217",
+            "991013628",
+            "993110469"
+        )
+    }
+
+    fun invalidNorwegianOrganizationNumbers(): List<String> {
+        return listOf(
+            "889640780",
+            "135795310",
+            "135795311",
+            "135795312",
+            "135795314",
+            "135795315",
+            "135795316",
+            "135795317",
+            "135795318",
+            "135795319"
+        )
+    }
 }
